16c1b26119e0a21a62c4ba8a0f8734828f07d5f2
manish2428 <manishyadav2056107@gmail.com>
2025-04-27 17:49:51 +0530

    Dependency inversion principle

---
+++ SOLID PRINCIPLE/DependencyInversionPrinciple.py
@@ -0,0 +1,50 @@
+# "High-level modules should not depend on low-level modules directly."
+# class MySqlDB:
+
+#     def connect(self):
+#         print("Connecting to MySqlDB")
+
+# class App:
+#     def __init__(self):
+#         self.db = MySqlDB()
+    
+#     def start(self):
+#         self.db.connect()
+
+# app = App()
+# app.start()
+
+
+# Problem:
+# App is tightly coupled with MySQL.
+
+
+from abc import ABC, abstractmethod
+class Database:
+    @abstractmethod
+    def connect(self):
+        pass
+
+class MySqlDB(Database):
+    def connect(self):
+        print(f"Connected to {self.__class__.__name__}")
+
+class PostgresqlDB(Database):
+    def connect(self):
+        print(f"Connected to {self.__class__.__name__}")
+
+class App:
+    def __init__(self, database: Database):
+        self.db = database
+
+    def start(self):
+        self.db.connect()
+
+mysql = MySqlDB()
+postgre = PostgresqlDB()
+sql = App(mysql)
+sql.start()
+post = App(postgre)
+post.start()
+
+        
